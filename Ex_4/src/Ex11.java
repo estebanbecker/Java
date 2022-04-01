feb330b91edec7f7a6a79e057836c8b186660bd2
Esteban Becker <git@beckeresteban.eu>
2022-04-01 09:14:34 +0200

    Hey

--- Ex_4/src/Ex11.java
+++ Ex_4/src/Ex11.java
@@ -1,5 +1,22 @@
 public class Ex11 {
-    public static void main (String[] args) {
-        
+
+    public void printHello(int i) {
+        System.out.println("Hello " + i);
     }
+    public static void main (String[] args) {
+        //Depending on the value provided from the command line, your program should create the exact number of objects.
+
+        //Each object should print short hello message on the screen, including the number of already created objects.
+
+        int numberOfObjects = Integer.parseInt(args[0]);
+
+        for (int i=0; i<numberOfObjects; i++) {
+            new Ex11().printHello(i+1);
+        }
+
+    }
+
+
 }
+
+
---
+++ Ex_7/src/G0.java
@@ -0,0 +1,25 @@
+package tomek.it.graphics;
+
+import javax.swing.JFrame;
+
+public class G0 {
+
+    public static void createAndShowGUI() {
+        JFrame jf = new JFrame("My First Frame");
+
+        jf.pack();
+        jf.setVisible(true);
+    }
+
+	
+    public static void main(String[] args) {
+        System.out.println("Before");
+        
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() { createAndShowGUI(); }
+        });
+        
+        System.out.println("After");
+    }
+
+}
