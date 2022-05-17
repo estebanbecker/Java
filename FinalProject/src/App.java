85f550abb8e0b45d45098f10d07410429e15e92c
Placard <git@beckeresteban.eu>
2022-05-17 18:47:16 +0200

    Recherche de films

--- FinalProject/src/App.java
+++ FinalProject/src/App.java
@@ -1,9 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.awt.*;
+import javax.swing.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+import java.awt.BorderLayout;
+import java.awt.Image;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
 
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
@@ -11,53 +23,145 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner myObj = new Scanner(System.in);
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-        JFrame frame = new JFrame("TMDB");
+public class App {
+
+    static JFrame frame = new JFrame("TMDB");
+    
+    static JTextField textField = new JTextField(20);
+    static JButton button = new JButton("Search");
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService pool = Executors.newFixedThreadPool(5);
+        
+        GridLayout grid = new GridLayout(1, 3);
+        JPanel panel = new JPanel(grid);
+
+        JLabel label = new JLabel("Enter movie name:");
+
+
+        
+
+        //Create ActionListener
+        ActionListener listener = new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                button.setText("Searching...");
+                pool.submit(new Task());            
+            }
+
+        };
+
+        textField.addActionListener(listener);
+        button.addActionListener(listener);
+
+        panel.add(label);
+        panel.add(textField);
+        panel.add(button);
+
+        frame.getContentPane().add(BorderLayout.NORTH, panel);
+        
+        frame.pack();
+        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Label label = new Label("Enter a movie title:");
-        TextField textField = new TextField();
 
-        String name = myObj.nextLine();
+        }
 
+        
+}
+class Task implements Runnable {
+
+    private static int number;
+	int l;
+	public Task() {
+		l=++number;
+	}
+    public void run() {
+        String name = App.textField.getText();
         String url = "https://www.themoviedb.org/search/movie?language=en&query=" + name;
-
-        try{
-            Document doc = Jsoup.connect(url).get();
-        } catch (HttpStatusException ex) {
-            System.out.println("Error: " + ex.getMessage());
-            //Quit
-            System.exit(0);
-        }
-        Document doc = Jsoup.connect(url).get();
-        
-        Elements movieresults = doc.getElementsByClass("card v4 tight");
-
-        List movieList = new ArrayList<Movie>();
-        
-        for (Element movieresult : movieresults) {
-            Movie m = new Movie();
-
-            m.setName(movieresult.getElementsByClass("result").text());
-            m.setReleaseDate(movieresult.getElementsByClass("release_date").text());
-            m.setDescription(movieresult.getElementsByClass("overview").text());
-            m.setImage(movieresult.getElementsByClass("poster").attr("src"));
-            m.setUrl(movieresult.getElementsByClass("result").attr("href"));
-
-            System.out.println(m.getName());
-            System.out.println(m.getReleaseDate());
-            System.out.println(m.getDescription());
-            System.out.println(m.getImage());
-            System.out.println(m.getUrl());
-
+            Document doc = null;
+            try{
+                doc = Jsoup.connect(url).get();
+            } catch ( IOException ex) {
+                System.out.println("Error: " + ex.getMessage());
+                //Quit
+                System.exit(0);
+            }
             
-        }
+            Elements movieresults = doc.getElementsByClass("card v4 tight");
 
+            GridLayout grid2 = new GridLayout(movieresults.size(), 1);
+            JPanel panel2 = new JPanel(grid2);
+            
+            URL urlimage = null;
+            Image image = null;
+            for (Element movieresult : movieresults) {
+                Movie m = new Movie();
+
+                GridLayout gridmovie = new GridLayout(1, 4);
+                JPanel panelmovie = new JPanel(gridmovie);
+
+                m.setName(movieresult.getElementsByClass("result").text());
+                m.setReleaseDate(movieresult.getElementsByClass("release_date").text());
+                m.setDescription(movieresult.getElementsByClass("overview").text());
+                m.setImage(movieresult.getElementsByClass("poster").attr("src"));
+                m.setUrl(movieresult.getElementsByClass("result").attr("href"));
+
+                System.out.println(m.getName());
+                System.out.println(m.getReleaseDate());
+                System.out.println(m.getDescription());
+                System.out.println(m.getImage());
+                System.out.println(m.getUrl());
+
+                try{
+                    
+                    urlimage = new URL("https://www.themoviedb.org"+m.getImage());
+                    //urlimage =new URL("https://www.themoviedb.org/t/p/w94_and_h141_bestv2/x2kzCjYqAPkqLSbv29fBQKE33Fl.jpg");
+                    System.out.println(urlimage);
+                    image = ImageIO.read(urlimage);
+                    if(image != null){
+                        JLabel label = new JLabel(new ImageIcon(image));
+                        panelmovie.add(label);
+                    }else{
+                        JLabel label = new JLabel("No image");
+                        panelmovie.add(label);
+                    }
+                } catch (MalformedURLException ex) {
+                    System.out.println("Malformed URL");
+                } catch (IOException iox) {
+                    System.out.println("Can not load file");
+                }
+                
+                
+
+                JLabel labelmovie = new JLabel(m.getName());
+                panelmovie.add(labelmovie);
+                if(m.getReleaseDate() != null){
+                    JLabel labelreleasedate = new JLabel(m.getReleaseDate());
+                    panelmovie.add(labelreleasedate);
+                }else{
+                    JLabel labelreleasedate = new JLabel("No release date");
+                    panelmovie.add(labelreleasedate);
+                }
+                JLabel labeldescription = new JLabel("<html>"+m.getDescription()+"</html>");
+                panelmovie.add(labeldescription);
+                
+                panel2.add(panelmovie);
+            }
+
+            App.frame.getContentPane().add(BorderLayout.CENTER, panel2);
+            App.frame.pack();
+            App.frame.setVisible(true);
+
+            App.button.setText("Search");
     }
 }
+
+
 
 class Movie {
     private String name;
