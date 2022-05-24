803b3303f3289fa96988ca25508883d6596b104c
Placard <git@beckeresteban.eu>
2022-05-24 19:35:48 +0200

    Clean

--- FinalProject/src/App.java
+++ FinalProject/src/App.java
@@ -205,7 +205,6 @@ class Task implements Runnable {
    
     public void run() {
         String name = App.textField.getText();
-        System.out.println("Searching for: " + name);
         String url = "https://www.themoviedb.org/search/movie?language=en&query=" + name;
             Document doc = null;
             try{
